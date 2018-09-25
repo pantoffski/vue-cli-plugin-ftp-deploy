911594083a56cdee503fcd1751986a524e283813
Chaichana Nilwatchararang <chaichana.n@gmail.com>
2018-09-25 13:20:33 +0700

    .

---
+++ generator/template/tools/.ftpdeploy.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  localBasePath:"/",
+  remoteBasePath: "/",
+  sync: [
+    { src: "/dist", dest: "/" }
+  ]
+};
---
+++ ui.js
@@ -0,0 +1,40 @@
+module.exports = api => {
+  api.addTask({
+    name: "FtpDeploy",
+    icon: "https://material.io/tools/icons/static/icons/baseline-account_balance-24px.svg",
+    command: "vue-cli-service ftpdeploy",
+    description: "ftp deploy to target server",
+    prompts: [
+      {
+        name: "ftpCfgPath",
+        type: "input",
+        default: "/tools/",
+        description: "`.ftpdeploy.config.js` location"
+      },
+      {
+        name: "ftpHistPath",
+        type: "input",
+        default: "/tools/",
+        description: "path to write `.ftpdeploy.hist.json`"
+      },
+      {
+        name: "genHist",
+        type: "confirm",
+        default: "true",
+        description: "gen file checksum"
+      },
+      {
+        name: "diffFileOnly",
+        type: "confirm",
+        default: "true",
+        description: "upload diff file from hist only"
+      }
+    ],
+    onBeforeRun: async ({ answers, args }) => {
+      if (answers.genHist) args.push("--genHist");
+      if (answers.diffFileOnly) args.push("--diffFileOnly");
+      if (answers.ftpCfgPath) args.push("--ftpCfgPath", answers.ftpCfgPath);
+      if (answers.ftpHistPath) args.push("--ftpHistPath", answers.ftpHistPath);
+    }
+  });
+};
